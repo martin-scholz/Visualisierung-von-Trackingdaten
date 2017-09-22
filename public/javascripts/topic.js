b7f70a1ee42c3ec8cfd64b3da0c582a553e24ec1
martin-scholz <m.kofi@gmx.de>
2017-09-22 08:47:05 +0200

    picsurl

--- public/javascripts/topic.js
+++ public/javascripts/topic.js
@@ -38,21 +38,21 @@
 
 
       var blueIcon = new L.Icon({
-        iconUrl: 'http://localhost:5000/images/marker-icon-2x-blue.png',
+        iconUrl: 'https://localhost:5000/images/marker-icon-2x-blue.png',
         iconSize: [25, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
         shadowSize: [41, 41]
         });
       var redIcon = new L.Icon({
-        iconUrl: 'http://localhost:5000/images/marker-icon-2x-red.png',
+        iconUrl: 'https://localhost:5000/images/marker-icon-2x-red.png',
         iconSize: [25, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
         shadowSize: [41, 41]
         });
       var yellowIcon = new L.Icon({
-        iconUrl: 'http://localhost:5000/images/marker-icon-2x-yellow.png',
+        iconUrl: 'https://localhost:5000/images/marker-icon-2x-yellow.png',
         iconSize: [25, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
